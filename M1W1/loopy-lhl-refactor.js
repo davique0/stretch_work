6f87cb4a0805528e50eface42e20630d238a37dc
davique0 <davique@gmail.com>
2022-12-22 22:45:01 -0500

    refactored

--- M1W1/loopy-lhl-refactor.js
+++ M1W1/loopy-lhl-refactor.js
@@ -12,14 +12,30 @@
 //   }
 // }
 
+// const loopyLighthouse = (range, multiples, words) => {
+//   let newArr = [];
+//   for (let i = range[0]; i <= range[1]; i++) {
+//     newArr.push(i)
+//   }
+//   newArr.map(x => {
+//     x % multiples[0] === 0 && x % multiples[1] === 0 ? console.log(`${words[0]} ${words[1]}`) : x % multiples[0] === 0 ? console.log(words[0]) : x % multiples[1] === 0 ? console.log(words[1]) : console.log(x)
+//   });
+// }
+
+// const loopyLighthouse = (range, multiples, words) => {
+//   for (let i = range[0]; i <= range[1]; i++) {
+//     if (!(i % multiples[0]) && !(i % multiples[1])) console.log(`${words[0]} ${words[1]}`);
+//     if (!(i % multiples[0])) console.log(words[0]);
+//     if (!(i % multiples[1])) console.log(words[1]);
+//     if ((i % multiples[0]) && (i % multiples[1])) console.log(i);
+//   }
+  
+// }
+
 const loopyLighthouse = (range, multiples, words) => {
-  let newArr = [];
   for (let i = range[0]; i <= range[1]; i++) {
-    newArr.push(i)
+    console.log((i % multiples[0] ? '' : words[0]) + (i % multiples[1] ? '' : words[1]) || i);
   }
-  newArr.map(x => {
-    x % multiples[0] === 0 && x % multiples[1] === 0 ? console.log(`${words[0]} ${words[1]}`) : x % multiples[0] === 0 ? console.log(words[0]) : x % multiples[1] === 0 ? console.log(words[1]) : console.log(x)
-  });
 }
 
-loopyLighthouse([0, 30], [3, 7], ['Mario', 'David']);
+loopyLighthouse([5, 40], [5, 7], ['Mario', 'David']);
